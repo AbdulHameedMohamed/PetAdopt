21bed435743d1a2f8c6cf73137b079428d0aee76
Abdsayed <Abdelhameed.sayed.ext@orange.com>
2025-05-19 10:52:41 +0300

    Testing The Domain Layer (Photo Logic).

---
+++ app/src/test/java/com/example/animals/domain/model/animal/PhotoTests.kt
@@ -0,0 +1,63 @@
+package com.example.animals.domain.model.animal
+
+import com.example.petadopt.animals.domain.model.animal.Media
+import org.junit.Test
+import org.junit.Assert.assertEquals
+
+class PhotoTests {
+    private val mediumPhoto = "mediumPhoto"
+    private val fullPhoto = "fullPhoto"
+    private val invalidPhoto = ""
+
+    @Test
+    fun photo_getSmallestAvailablePhoto_hasMediumPhoto() {
+        // Given
+        val photo = Media.Photo(mediumPhoto, fullPhoto)
+        val expectedValue = mediumPhoto
+
+        // When
+        val smallestPhoto = photo.getSmallestAvailablePhoto()
+
+        // Then
+        assertEquals(smallestPhoto, expectedValue)
+    }
+
+    @Test
+    fun photo_getSmallestAvailablePhoto_notHasLargePhoto() {
+        // Given
+        val photo = Media.Photo(mediumPhoto, "")
+        val expectedValue = mediumPhoto
+
+        // When
+        val smallestPhoto = photo.getSmallestAvailablePhoto()
+
+        // Then
+        assertEquals(smallestPhoto, expectedValue)
+    }
+
+    @Test
+    fun photo_getSmallestAvailablePhoto_noMediumPhoto_hasFullPhoto() {
+        // Given
+        val photo = Media.Photo(invalidPhoto, fullPhoto)
+        val expectedValue = fullPhoto
+
+        // When
+        val smallestPhoto = photo.getSmallestAvailablePhoto()
+
+        // Then
+        assertEquals(smallestPhoto, expectedValue)
+    }
+
+    @Test
+    fun photo_getSmallestAvailablePhoto_noPhotos() {
+        // Given
+        val photo = Media.Photo(invalidPhoto, invalidPhoto)
+        val expectedValue = Media.Photo.EMPTY_PHOTO
+
+        // When
+        val smallestPhoto = photo.getSmallestAvailablePhoto()
+
+        // Then
+        assertEquals(smallestPhoto, expectedValue)
+    }
+}
